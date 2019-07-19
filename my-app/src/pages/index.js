213c40a4c57d88cd0de1f3c9b643a70e7e6e906e
lavender_duan <guanghua@bluescorpion.cn>
2019-07-19 15:47:23 +0800

    #

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,20 @@
+import React,{PureComponent} from 'react';
+import NewCompoment from '../components/demo'
+
+@NewCompoment
+class Index extends PureComponent {
+     
+    constructor(props){
+        super(props)
+        this.state={}
+    }
+
+    render(){
+
+        return (<div>Hello React</div>)
+
+    }
+
+}
+
+export default Index
